e42a73945c4cb01c6f3c9781b306093db9d3183a
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-08-21 22:29:19 -0400

    init

---
+++ common/src/main/java/earth/terrarium/potmeetkettle/PotMeetKettle.java
@@ -0,0 +1,9 @@
+package earth.terrarium.potmeetkettle;
+
+public class PotMeetKettle {
+    public static final String MOD_ID = "potmeetkettle";
+
+    public static void init() {
+
+    }
+}
---
+++ fabric/src/main/java/earth/terrarium/potmeetkettle/fabric/PotMeetKettleFabric.java
@@ -0,0 +1,11 @@
+package earth.terrarium.potmeetkettle.fabric;
+
+import earth.terrarium.potmeetkettle.PotMeetKettle;
+import net.fabricmc.api.ModInitializer;
+
+public class PotMeetKettleFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        PotMeetKettle.init();
+    }
+}
